1fa9b0b03d697e950e4fa193e33adbede436af71
cybershen <cyberpuer@proton.me>
2023-02-14 21:33:43 +0200

    Add display for LoginViewController. Also add MainTabBarController's screen

--- MoviePlay/Controllers/Core/LoginViewController.swift
+++ MoviePlay/Controllers/Core/LoginViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    
+    @IBAction func loginTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let mainTabBarVC = storyboard.instantiateViewController(withIdentifier: "MainTabBarController") as! MainTabBarViewController
+        navigationController?.pushViewController(mainTabBarVC, animated: true)
     }
 }
